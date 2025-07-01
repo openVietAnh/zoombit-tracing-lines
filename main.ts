3c7a9c453af95cc3483e30b5b096ae78bee58bcf
Viet Anh Tran <42197149+openVietAnh@users.noreply.github.com>
2025-07-01 22:27:41 +0700

    add Shape of you rhythm, and moving forward until obstacles detected

--- main.ts
+++ main.ts
@@ -1,19 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    music.setTempo(140)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+})
 input.onSound(DetectedSound.Loud, function () {
     zoombit.toggleHeadlight(HeadlightChannel.All)
 })
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    if (zoombit.isLineDetectedOn(LinePosition.Center)) {
-        zoombit.move(MotorDirection.Forward, 128)
-    } else if (zoombit.isLineDetectedOn(LinePosition.Left1)) {
-        zoombit.setMotorsSpeed(50, 100)
-    } else if (zoombit.isLineDetectedOn(LinePosition.Right1)) {
-        zoombit.setMotorsSpeed(100, 50)
-    } else if (zoombit.isLineDetectedOn(LinePosition.Left2)) {
-        zoombit.setMotorsSpeed(0, 100)
-    } else if (zoombit.isLineDetectedOn(LinePosition.Right2)) {
-        zoombit.setMotorsSpeed(100, 0)
+    if (zoombit.readUltrasonic() > 10) {
+        zoombit.setMotorsSpeed(100, 100)
     } else {
-    	
+        zoombit.brake()
     }
 })
