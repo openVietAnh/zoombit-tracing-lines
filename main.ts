7d7d9df9b8009e7c03f3e924ac37c3077cacb40a
Viet Anh Tran <42197149+openVietAnh@users.noreply.github.com>
2025-07-02 22:53:09 +0700

    combine all codes

--- main.ts
+++ main.ts
@@ -27,11 +27,31 @@ input.onButtonPressed(Button.A, function () {
 input.onSound(DetectedSound.Loud, function () {
     zoombit.toggleHeadlight(HeadlightChannel.All)
 })
+input.onButtonPressed(Button.AB, function () {
+    if (zoombit.isLineDetectedOn(LinePosition.Center)) {
+        zoombit.move(MotorDirection.Forward, 128)
+    } else if (zoombit.isLineDetectedOn(LinePosition.Left1)) {
+        zoombit.setMotorsSpeed(50, 100)
+    } else if (zoombit.isLineDetectedOn(LinePosition.Right1)) {
+        zoombit.setMotorsSpeed(100, 50)
+    } else if (zoombit.isLineDetectedOn(LinePosition.Left2)) {
+        zoombit.setMotorsSpeed(0, 100)
+    } else if (zoombit.isLineDetectedOn(LinePosition.Right2)) {
+        zoombit.setMotorsSpeed(100, 0)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        if (zoombit.readUltrasonic() > 10) {
+            zoombit.setMotorsSpeed(230, 255)
+        } else {
+            zoombit.brake()
+        }
+    }
+})
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    if (zoombit.readUltrasonic() > 10) {
-        zoombit.setMotorsSpeed(100, 100)
-    } else {
-        zoombit.brake()
-    }
+	
 })
